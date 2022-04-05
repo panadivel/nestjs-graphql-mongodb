f09218907845825d68ea44343f85ea944045d3a2
panadivel <panadivel.v2@gmail.com>
2022-04-06 00:15:45 +0300

    add apollo driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
-
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
+      driver: ApolloDriver,
     }),
     LessonModule,
   ],
