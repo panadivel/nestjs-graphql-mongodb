795e531ce0477948f1a9f9750f078c49e0aa9393
panadivel <panadivel.v2@gmail.com>
2022-04-09 23:36:20 +0300

    add getLessons service

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -6,9 +6,15 @@ import { LessonType } from './lesson.type';
 @Resolver((of) => LessonType)
 export class LessonResolver {
   constructor(private lessonService: LessonService) {}
+
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string) {
     return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  lessons() {
+    return this.lessonService.getLessons();
   }
 
   @Mutation((returns) => LessonType)
