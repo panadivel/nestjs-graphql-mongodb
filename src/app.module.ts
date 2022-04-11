70987dc8438ae10003e40449da81fdb2ea0d8e62
panadivel <panadivel.v2@gmail.com>
2022-04-11 22:28:00 +0300

    add student module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -11,13 +13,14 @@ import { Lesson } from './lesson/lesson.entity';
       url: 'mongodb://localhost:27017/mongodb_nest',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
     }),
     LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule {}
