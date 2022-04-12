17b5d67863c51cd3cd5ced9afab9f56919fb1f3c
panadivel <panadivel.v2@gmail.com>
2022-04-12 22:30:33 +0300

    add createStudent method

---
+++ src/student/student.service.ts
@@ -0,0 +1,28 @@
+import { v4 as uuid } from 'uuid';
+
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student) private studentRepository: Repository<Student>,
+  ) {}
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return this.studentRepository.save(student);
+  }
+}
