54235ff9f8a093d7f0f4a78d3fcb4928127f3fcd
panadivel <panadivel.v2@gmail.com>
2022-04-13 21:11:50 +0300

    add getStudents service

--- src/student/student.resolver.ts
+++ src/student/student.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { CreateStudentInput } from './create-student.input';
 import { StudentType } from './student.type';
 import { StudentService } from './student.service';
@@ -6,6 +6,11 @@ import { StudentService } from './student.service';
 @Resolver((of) => StudentType)
 export class StudentResolver {
   constructor(private studentService: StudentService) {}
+
+  @Query((returns) => [StudentType])
+  async students() {
+    return this.studentService.getStudents();
+  }
 
   @Mutation((returns) => StudentType)
   createStudent(
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -25,4 +25,8 @@ export class StudentService {
 
     return this.studentRepository.save(student);
   }
+
+  async getStudents(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
 }
